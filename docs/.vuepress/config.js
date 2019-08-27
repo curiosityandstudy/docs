f7731e81164926088a6e248c4164515f5f92ceff
Shenwei Min <1152764252@qq.com>
2019-08-27 18:42:31 +0800

    fix

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    evergreen: true,
+    title: '个人主页', 
+    description: 'msw的博客',
+    head: [
+        ['link', { rel: 'icon', href: '/.vuepress/public/2.jpg' }],
+    ],
+
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/other/tryother.md' },
+          { text: 'External', link: 'https://google.com' },
+        ],
+
+        sidebar: {
+            '/other/': [
+                "/other/tryother.md",
+                ""
+            ]
+        }
+
+      }
+}
