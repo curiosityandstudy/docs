d384ef06391f5ca6546219629c40fd076c7f9810
Shenwei Min <1152764252@qq.com>
2019-08-28 10:08:52 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,11 @@
 module.exports = {
     evergreen: true,
-    title: '个人主页', 
-    description: 'msw的博客',
+   
     themeConfig: {
         nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Guide', link: '/other/tryother.md' },
-          { text: 'External', link: 'https://google.com' },
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/other/tryother.md' },
+            { text: 'External', link: 'https://google.com' },
         ],
 
         sidebar: {
@@ -15,5 +14,50 @@ module.exports = {
             ]
         }
 
-      }
+    }
+}
+
+module.exports = {
+    evergreen: true,
+    title: '个人主页',
+    description: 'msw的博客',
+    locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: '掌中云测试文档',
+            description: ''
+        }
+    },
+    themeConfig: {
+        sidebarDepth: 3,
+        locales: {
+            '/': {
+                label: '简体中文',
+                selectText: '选择语言',
+                lastUpdated: '上次编辑时间',
+                sidebar: {
+                    '/': [
+                        {
+                            title: '基本',
+                            children: [
+                                '/other/tryother.md'
+                            ]
+                        },
+                        {
+                            title: '测试1',
+                            children: [
+                                '/test1/try1.md'
+                            ]
+                        },
+                        {
+                            title: '测试2',
+                            children: [
+                                '/test2/try2.md'
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
 }
